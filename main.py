a7542d1ff302c96a31776a3b3f4e6ffc2e7825f3
ramzi <rhosisey@augury.com>
2018-02-13 00:24:59 +0200

    set basic class

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+class Training_1():
+
+    def print_time_console(self):
+
+        print 'the time is {}'.format(time.time())
+
+
---
+++ test_1/first-training.py
@@ -0,0 +1,8 @@
+
+class FirstTrainig():
+
+    def print_on_console(self):
+        print ''
+
+
+
